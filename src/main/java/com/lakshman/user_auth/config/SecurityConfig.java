2fc9d7fb52621fe1cdd956f55e058374317207f5
Lakshman Bhupathi <lakshmanbhupathi@gmail.com>
2025-11-11 16:53:18 +0530

    main: Added secure config

---
+++ src/main/java/com/lakshman/user_auth/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.lakshman.user_auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> {
+                    auth
+                    .requestMatchers("/api/users/**").authenticated()
+                    .anyRequest().permitAll();
+                })
+                .build();
+    }
+
+
+}
